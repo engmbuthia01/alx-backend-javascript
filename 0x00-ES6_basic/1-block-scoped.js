84c0b92c4cc9c3e08771cfd96b1de21c82364e7b
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-09-30 13:58:07 +0000

    using const to ensure variables are not changed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; //const for bloack scope
+  const task2 = true; //const for block scope
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true; //updating existing variable
+    const task2 = false; // updating existing variable
   }
 
   return [task, task2];
